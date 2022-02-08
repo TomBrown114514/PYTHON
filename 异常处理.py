ef3061c395e7ec5d4e7cc222e21a5d10a04a00f4
Tom <tomb0968@outlook.com>
2022-02-09 07:56:26 +0800

    更改断言测试的条件

--- 异常处理.py
+++ 异常处理.py
@@ -4,7 +4,7 @@ def division():
     apple = int(input("请输入苹果的个数:")) #输入苹果的个数
     children = int(input("请输入来了几个小朋友:"))
     assert apple > children, "苹果不够分" #应用断言测试
-    assert children != 0, "不能被零个人分!" 
+    assert children > 0, "不能被零或负数个人分!" 
     result = apple // children #计算每个人分几个苹果
     remain = apple % children #计算余下的几个苹果
     print(f"{apple}个苹果,平均分给{children}个小朋友,每人分{result}个,剩下{remain}个" if remain > 0\
