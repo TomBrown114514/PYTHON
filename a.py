78bfbce4df3f2de27d5b36251f7c30ae63d0920c
Tom <tomb0968@outlook.com>
2022-02-10 09:02:56 +0800

    gg

--- a.py
+++ a.py
@@ -1,14 +1,14 @@
 #使用版本：Python 3.11.0a5
-def fbnq(n: int) -> int :
+def fbid(n: int) -> int :
     match n:
         case 1|2:
             return 1
         case _:
-            return fbnq(n-2)+fbnq(n-1)
+            return fbid(n-2)+fbid(n-1)
 
 try:
     n = int(input("请输入一个正整数:"))
     assert n > 0, "请输入一个正整数!"
-    print(f'第{n}项是{fbnq(n)}')
+    print(f'第{n}项是{fbid(n)}')
 except AssertionError as ae:
     print(ae)
